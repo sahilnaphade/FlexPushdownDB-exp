75f25a69eab4375fe84b252b544a1aed1225aac5
Yifei Yang <yiy032@eng.ucsd.edu>
2021-12-01 23:15:50 -0600

    (Saving...) working on tpch q01

--- resources/query/tpch/original/19.sql
+++ resources/query/tpch/original/19.sql
@@ -5,10 +5,9 @@ from
   cp."tpch/lineitem.parquet" l,
   cp."tpch/part.parquet" p
 where
-    p.p_partkey = l.l_partkey
-    and (
   (
-    p.p_brand = 'Brand#41'
+    p.p_partkey = l.l_partkey
+    and p.p_brand = 'Brand#41'
     and p.p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
     and l.l_quantity >= 2 and l.l_quantity <= 2 + 10
     and p.p_size between 1 and 5
@@ -17,7 +16,8 @@ where
   )
   or
   (
-    p.p_brand = 'Brand#13'
+    p.p_partkey = l.l_partkey
+    and p.p_brand = 'Brand#13'
     and p.p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
     and l.l_quantity >= 14 and l.l_quantity <= 14 + 10
     and p.p_size between 1 and 10
@@ -26,10 +26,11 @@ where
   )
   or
   (
-    p.p_brand = 'Brand#55'
+    p.p_partkey = l.l_partkey
+    and p.p_brand = 'Brand#55'
     and p.p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
     and l.l_quantity >= 23 and l.l_quantity <= 23 + 10
     and p.p_size between 1 and 15
     and l.l_shipmode in ('AIR', 'AIR REG')
     and l.l_shipinstruct = 'DELIVER IN PERSON'
-  ) );
+  );
