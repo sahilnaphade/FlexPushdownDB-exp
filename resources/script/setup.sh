dd7a8ec42ff281c7fb6547e785c9ea7897168e58
Yifei Yang <yiy032@eng.ucsd.edu>
2022-01-24 22:26:15 -0600

    (Saving...) Writing scripts for deployment...

--- resources/script/setup.sh
+++ resources/script/setup.sh
@@ -1,1 +1,12 @@
 # main entry to setup the system on a cluster
+
+# get script path
+pushd "$(dirname "$0")" > /dev/null
+script_dir=$(pwd)
+popd > /dev/null
+
+# build
+source "$script_dir""/build.sh"
+
+# deploy
+source "$script_dir""/deploy.sh"
