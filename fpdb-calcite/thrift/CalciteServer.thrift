0bcfabfdc75a191848993ae5b47b850f1ac04496
Yifei Yang <yiy032@eng.ucsd.edu>
2022-02-06 21:51:36 -0600

    Fix typos.

--- fpdb-calcite/thrift/CalciteServer.thrift
+++ fpdb-calcite/thrift/CalciteServer.thrift
@@ -12,6 +12,6 @@ struct TPlanResult {
 service CalciteServer {
   void ping(),
   void shutdown(),
-  TPlanResult sql2Plan(1: string query 2: string schemaName) throws (1: ParsePlanningError parsePlanningErr),
+  TPlanResult sql2Plan(1: string query, 2: string schemaName) throws (1: ParsePlanningError parsePlanningErr),
   void updateMetadata(1: string catalog, 2: string table)
 }
