4fadd83a339dae6daaa728b5be14afabc762566d
Matt Youill <matt.youill@burnian.com>
2022-02-25 10:33:08 +1100

    Fix libbfd not loading properly

--- cmake/FindLibBFD.cmake
+++ cmake/FindLibBFD.cmake
@@ -9,16 +9,20 @@ find_library(LibBFD_LIBRARY
         bfd
         PATHS /usr/${CMAKE_INSTALL_LIBDIR} /usr/local/opt/binutils/${CMAKE_INSTALL_LIBDIR})
 
-set(LibBFD_INCLUDE_DIRS ${LibBFD_INCLUDE_DIR} ${LibDL_INCLUDE_DIR})
-set(LibBFD_LIBRARIES ${LibBFD_LIBRARY} ${LibDL_LIBRARY})
+set(LibBFD_INCLUDE_DIRS ${LibBFD_INCLUDE_DIR})
+set(LibBFD_LIBRARIES ${LibBFD_LIBRARY})
 
-find_package_handle_standard_args(LibBFD ${_name_mismatched_arg}
+find_package_handle_standard_args(LibBFD
         REQUIRED_VARS LibBFD_LIBRARY LibBFD_INCLUDE_DIR)
 
 if(LibBFD_FOUND)
+
+    find_package(LibOpcodes REQUIRED)
+
     add_library(LibBFD::LibBFD UNKNOWN IMPORTED)
     set_target_properties(LibBFD::LibBFD PROPERTIES IMPORTED_LOCATION ${LibBFD_LIBRARY})
     target_include_directories(LibBFD::LibBFD INTERFACE ${LibBFD_INCLUDE_DIR})
+    target_link_libraries(LibBFD::LibBFD INTERFACE LibOpcodes::LibOpcodes)
     target_link_libraries(LibBFD::LibBFD INTERFACE ${CMAKE_DL_LIBS})
 else()
     message(FATAL_ERROR "BFD library not found")
