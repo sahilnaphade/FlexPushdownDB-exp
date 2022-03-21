53e58292d37eaeb768498b197d90e974c999d2ff
Yifei Yang <yiy032@eng.ucsd.edu>
2022-03-21 17:09:38 -0500

    script to copy data from s3 to local ssd

---
+++ resources/script/fpdb-store/prepare_data_s3.sh
@@ -0,0 +1,30 @@
+# script to prepare data in storage nodes, when a new instance is started
+# including mounting the nvme-ssd storage and downloading data from S3
+# need aws credentials already set
+
+# configurable parameters
+bucket="flexpushdowndb"
+data_relative_dirs=("tpch-sf10")
+mount_point="/fpdb-store"
+storage_name="/dev/nvme1n1"
+temp_dir="$HOME""/""temp"
+
+# mount nvme-ssd to the mount point and set access
+sudo umount "$storage_name"
+sudo mkdir -p "$mount_point"
+
+sudo mkfs.ext4 -E nodiscard "$storage_name"
+sudo mount "$storage_name" "$mount_point"
+
+bucket_dir="$mount_point""/""$bucket"
+sudo mkdir -p "$bucket_dir"
+sudo chown -R ubuntu:ubuntu "$mount_point"
+
+# download data from S3
+for data_relative_dir in "${data_relative_dirs[@]}"
+do
+  aws s3 cp --recursive "s3://""$bucket""/""$data_relative_dir" "$temp_dir""/""$data_relative_dir"
+  mv "$temp_dir""/""$data_relative_dir" "$bucket_dir""/""$data_relative_dir"
+done
+
+sudo rm -rf "$temp_dir"
