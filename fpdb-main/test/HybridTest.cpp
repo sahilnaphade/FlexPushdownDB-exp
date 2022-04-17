8f9a7ae200463f2379e956f840b91caba33d597d
Yifei Yang <yiy032@eng.ucsd.edu>
2022-04-17 18:34:38 -0500

    Add inseparable case for hybrid test

--- fpdb-main/test/HybridTest.cpp
+++ fpdb-main/test/HybridTest.cpp
@@ -106,6 +106,59 @@ TEST_CASE ("hybrid-none-cached" * doctest::skip(false || SKIP_SUITE)) {
   TestUtil::removeQueryFile(testQueryFileName);
 }
 
+TEST_CASE ("hybrid-partial-cached-filter-inseparable" * doctest::skip(false || SKIP_SUITE)) {
+  std::string cachingQueryFileName = "caching.sql";
+  std::string testQueryFileName = "test.sql";
+  std::string cachingQuery = "select\n"
+                             "    l_returnflag, l_quantity\n"
+                             "from\n"
+                             "    lineitem\n"
+                             "where\n"
+                             "    l_quantity < 20\n"
+                             "order by\n"
+                             "    l_returnflag\n"
+                             "limit 10";
+  std::string testQuery = "select\n"
+                          "    l_returnflag, l_linestatus, l_quantity, l_extendedprice\n"
+                          "from\n"
+                          "    lineitem\n"
+                          "where\n"
+                          "    l_discount <= 0.02\n"
+                          "order by\n"
+                          "    l_returnflag, l_linestatus\n"
+                          "limit 10";
+
+  // write query to file
+  TestUtil::writeQueryToFile(cachingQueryFileName, cachingQuery);
+  TestUtil::writeQueryToFile(testQueryFileName, testQuery);
+
+  // test
+  startFPDBStoreServer();
+  TestUtil testUtil("tpch-sf0.01/parquet/",
+                    {cachingQueryFileName,
+                     testQueryFileName},
+                    PARALLEL_TPCH_FPDB_STORE_SAME_NODE,
+                    false,
+                    ObjStoreType::FPDB_STORE,
+                    Mode::hybridMode(),
+                    CachingPolicyType::LFU,
+                    1L * 1024 * 1024 * 1024);
+  // fix cache layout after caching query, otherwise it keeps fetching new segments
+  // which is not intra-partition hybrid execution (i.e. hit data + loaded new cache data is enough for execution,
+  // pushdown part actually does nothing)
+  testUtil.setFixLayoutIndices({0});
+
+  REQUIRE_NOTHROW(testUtil.runTest());
+  REQUIRE_GT(testUtil.getCrtQueryHitRatio(), 0.0);
+  REQUIRE_LT(testUtil.getCrtQueryHitRatio(), 1.0);
+
+  stopFPDBStoreServer();
+
+  // clear query file
+  TestUtil::removeQueryFile(cachingQueryFileName);
+  TestUtil::removeQueryFile(testQueryFileName);
+}
+
 TEST_CASE ("hybrid-partial-cached-filter-separable" * doctest::skip(false || SKIP_SUITE)) {
   std::string cachingQueryFileName = "caching.sql";
   std::string testQueryFileName = "test.sql";
