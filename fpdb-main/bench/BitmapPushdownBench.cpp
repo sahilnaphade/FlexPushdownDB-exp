5308329d2e1389f32feaa2c65c391adadf3059d8
Yifei Yang <yiy032@eng.ucsd.edu>
2022-04-18 13:26:58 -0500

    Add non bitmap pushdown runs for bitmap pushdown bench

--- fpdb-main/bench/BitmapPushdownBench.cpp
+++ fpdb-main/bench/BitmapPushdownBench.cpp
@@ -20,8 +20,7 @@ TEST_SUITE ("bitmap-pushdown-bench" * doctest::skip(SKIP_SUITE)) {
 int SF_BITMAP_PUSHDOWN_BENCH = 10;
 
 TEST_CASE ("bitmap-pushdown-bench-tpch-fpdb-store-diff-node-compute-bitmap" * doctest::skip(false || SKIP_SUITE)) {
-  fpdb::executor::physical::ENABLE_FPDB_STORE_BITMAP_PUSHDOWN = true;
-
+  std::vector<bool> enableBitMapPushdownConfigs = {true, false};
   std::string cachingQueryFileName = "caching.sql";
   std::string testQueryFileNameBase = "test_{}.sql";
 
@@ -34,50 +33,56 @@ TEST_CASE ("bitmap-pushdown-bench-tpch-fpdb-store-diff-node-compute-bitmap" * do
                                          "l_orderkey >= 0",
                                          "l_partkey >= 0"};
 
-  for (uint predicateNum = 1; predicateNum <= allPredicates.size(); ++predicateNum) {
-    // create queries
-    std::vector<std::string> predicates(allPredicates.begin(), allPredicates.begin() + predicateNum);
-    std::string cachingQuery = fmt::format("select\n"
-                                           "    l_discount\n"
-                                           "from\n"
-                                           "    lineitem\n"
-                                           "where\n"
-                                           "    {}\n", fmt::join(predicates, " and "));
-    std::string testQuery = fmt::format("select\n"
-                                        "    l_tax, l_extendedprice\n"
-                                        "from\n"
-                                        "    lineitem\n"
-                                        "where\n"
-                                        "    {}\n", fmt::join(predicates, " and "));
-    std::string testQueryFileName = fmt::format(testQueryFileNameBase, predicateNum);
-    TestUtil::writeQueryToFile(cachingQueryFileName, cachingQuery);
-    TestUtil::writeQueryToFile(testQueryFileName, testQuery);
+  for (bool enableBitMapPushdown: enableBitMapPushdownConfigs) {
+    std::cout << fmt::format("Enable Bitmap Pushdown: {}", enableBitMapPushdown) << std::endl;
+    fpdb::executor::physical::ENABLE_FPDB_STORE_BITMAP_PUSHDOWN = enableBitMapPushdown;
 
-    // run test
-    TestUtil testUtil(fmt::format("tpch-sf{}/parquet/", SF_BITMAP_PUSHDOWN_BENCH),
-                      {cachingQueryFileName,
-                       testQueryFileName},
-                      PARALLEL_TPCH_FPDB_STORE_SAME_NODE,
-                      false,
-                      ObjStoreType::FPDB_STORE,
-                      Mode::hybridMode(),
-                      CachingPolicyType::LFU,
-                      50L * 1024 * 1024 * 1024);
-    // fix cache layout after caching query, otherwise it keeps fetching new segments
-    // which is not intra-partition hybrid execution (i.e. hit data + loaded new cache data is enough for execution,
-    // pushdown part actually does nothing)
-    testUtil.setFixLayoutIndices({0});
-    REQUIRE_NOTHROW(testUtil.runTest());
+    for (uint predicateNum = 1; predicateNum <= allPredicates.size(); ++predicateNum) {
+      // create queries
+      std::vector<std::string> predicates(allPredicates.begin(), allPredicates.begin() + predicateNum);
+      std::string cachingQuery = fmt::format("select\n"
+                                             "    l_discount\n"
+                                             "from\n"
+                                             "    lineitem\n"
+                                             "where\n"
+                                             "    {}\n", fmt::join(predicates, " and "));
+      std::string testQuery = fmt::format("select\n"
+                                          "    l_tax, l_extendedprice\n"
+                                          "from\n"
+                                          "    lineitem\n"
+                                          "where\n"
+                                          "    {}\n", fmt::join(predicates, " and "));
+      std::string testQueryFileName = fmt::format(testQueryFileNameBase, predicateNum);
+      TestUtil::writeQueryToFile(cachingQueryFileName, cachingQuery);
+      TestUtil::writeQueryToFile(testQueryFileName, testQuery);
 
-    // delete queries
-    TestUtil::removeQueryFile(cachingQueryFileName);
-    TestUtil::removeQueryFile(testQueryFileName);
+      // run test
+      TestUtil testUtil(fmt::format("tpch-sf{}/parquet/", SF_BITMAP_PUSHDOWN_BENCH),
+                        {cachingQueryFileName,
+                         testQueryFileName},
+                        PARALLEL_TPCH_FPDB_STORE_SAME_NODE,
+                        false,
+                        ObjStoreType::FPDB_STORE,
+                        Mode::hybridMode(),
+                        CachingPolicyType::LFU,
+                        50L * 1024 * 1024 * 1024);
+      // fix cache layout after caching query, otherwise it keeps fetching new segments
+      // which is not intra-partition hybrid execution (i.e. hit data + loaded new cache data is enough for execution,
+      // pushdown part actually does nothing)
+      testUtil.setFixLayoutIndices({0});
+              REQUIRE_NOTHROW(testUtil.runTest());
+
+      // delete queries
+      TestUtil::removeQueryFile(cachingQueryFileName);
+      TestUtil::removeQueryFile(testQueryFileName);
+    }
   }
+
+  fpdb::executor::physical::ENABLE_FPDB_STORE_BITMAP_PUSHDOWN = false;
 }
 
 TEST_CASE ("bitmap-pushdown-bench-tpch-fpdb-store-diff-node-storage-bitmap" * doctest::skip(false || SKIP_SUITE)) {
-  fpdb::executor::physical::ENABLE_FPDB_STORE_BITMAP_PUSHDOWN = true;
-
+  std::vector<bool> enableBitMapPushdownConfigs = {true, false};
   std::string cachingQueryFileName = "caching.sql";
   std::string testQueryFileNameBase = "test_{}.sql";
 
@@ -90,45 +95,52 @@ TEST_CASE ("bitmap-pushdown-bench-tpch-fpdb-store-diff-node-storage-bitmap" * do
                                           "l_shipdate",
                                           "l_commitdate"};
 
-  for (uint projectColumnNum = 1; projectColumnNum <= projectColumns.size(); ++projectColumnNum) {
-    // create queries, test query with ~20% selectivity
-    std::vector<std::string> cachedProjectColumns(projectColumns.begin(), projectColumns.begin() + projectColumnNum);
-    std::string cachingQuery = fmt::format("select\n"
-                                           "    {}\n"
-                                           "from\n"
-                                           "    lineitem\n"
-                                           "where\n"
-                                           "    l_quantity < 20\n", fmt::join(cachedProjectColumns, ", "));
-    std::string testQuery = fmt::format("select\n"
-                                        "    {}\n"
-                                        "from\n"
-                                        "    lineitem\n"
-                                        "where\n"
-                                        "    l_discount <= 0.01\n", fmt::join(cachedProjectColumns, ", "));
-    std::string testQueryFileName = fmt::format(testQueryFileNameBase, projectColumnNum);
-    TestUtil::writeQueryToFile(cachingQueryFileName, cachingQuery);
-    TestUtil::writeQueryToFile(testQueryFileName, testQuery);
+  for (bool enableBitMapPushdown: enableBitMapPushdownConfigs) {
+    std::cout << fmt::format("Enable Bitmap Pushdown: {}", enableBitMapPushdown) << std::endl;
+    fpdb::executor::physical::ENABLE_FPDB_STORE_BITMAP_PUSHDOWN = enableBitMapPushdown;
 
-    // run test
-    TestUtil testUtil(fmt::format("tpch-sf{}/parquet/", SF_BITMAP_PUSHDOWN_BENCH),
-                      {cachingQueryFileName,
-                       testQueryFileName},
-                      PARALLEL_TPCH_FPDB_STORE_SAME_NODE,
-                      false,
-                      ObjStoreType::FPDB_STORE,
-                      Mode::hybridMode(),
-                      CachingPolicyType::LFU,
-                      50L * 1024 * 1024 * 1024);
-    // fix cache layout after caching query, otherwise it keeps fetching new segments
-    // which is not intra-partition hybrid execution (i.e. hit data + loaded new cache data is enough for execution,
-    // pushdown part actually does nothing)
-    testUtil.setFixLayoutIndices({0});
-    REQUIRE_NOTHROW(testUtil.runTest());
+    for (uint projectColumnNum = 1; projectColumnNum <= projectColumns.size(); ++projectColumnNum) {
+      // create queries, test query with ~20% selectivity
+      std::vector<std::string> cachedProjectColumns(projectColumns.begin(), projectColumns.begin() + projectColumnNum);
+      std::string cachingQuery = fmt::format("select\n"
+                                             "    {}\n"
+                                             "from\n"
+                                             "    lineitem\n"
+                                             "where\n"
+                                             "    l_quantity < 20\n", fmt::join(cachedProjectColumns, ", "));
+      std::string testQuery = fmt::format("select\n"
+                                          "    {}\n"
+                                          "from\n"
+                                          "    lineitem\n"
+                                          "where\n"
+                                          "    l_discount <= 0.01\n", fmt::join(cachedProjectColumns, ", "));
+      std::string testQueryFileName = fmt::format(testQueryFileNameBase, projectColumnNum);
+      TestUtil::writeQueryToFile(cachingQueryFileName, cachingQuery);
+      TestUtil::writeQueryToFile(testQueryFileName, testQuery);
 
-    // delete queries
-    TestUtil::removeQueryFile(cachingQueryFileName);
-    TestUtil::removeQueryFile(testQueryFileName);
+      // run test
+      TestUtil testUtil(fmt::format("tpch-sf{}/parquet/", SF_BITMAP_PUSHDOWN_BENCH),
+                        {cachingQueryFileName,
+                         testQueryFileName},
+                        PARALLEL_TPCH_FPDB_STORE_SAME_NODE,
+                        false,
+                        ObjStoreType::FPDB_STORE,
+                        Mode::hybridMode(),
+                        CachingPolicyType::LFU,
+                        50L * 1024 * 1024 * 1024);
+      // fix cache layout after caching query, otherwise it keeps fetching new segments
+      // which is not intra-partition hybrid execution (i.e. hit data + loaded new cache data is enough for execution,
+      // pushdown part actually does nothing)
+      testUtil.setFixLayoutIndices({0});
+              REQUIRE_NOTHROW(testUtil.runTest());
+
+      // delete queries
+      TestUtil::removeQueryFile(cachingQueryFileName);
+      TestUtil::removeQueryFile(testQueryFileName);
+    }
   }
+
+  fpdb::executor::physical::ENABLE_FPDB_STORE_BITMAP_PUSHDOWN = false;
 }
 
 }
