d03d7e259d22539469bd86ba96f2a96bc5c72aef
Yifei Yang <yiy032@eng.ucsd.edu>
2022-09-16 22:10:05 -0500

    write script to download data from s3 to a cluster of storage nodes

--- resources/script/fpdb-store/prepare_cluster_data_from_s3.sh
+++ resources/script/fpdb-store/prepare_cluster_data_from_s3.sh
@@ -9,10 +9,23 @@ source "$util_func_path"
 # configurable parameters
 data_relative_dirs=("tpch-sf0.01")
 
-# download data on each storage node
-for node_ip in "${node_ips[@]}"
+# download data on each storage node, for downloading each dir, nodes can do in parallel
+for data_relative_dir in "${data_relative_dirs[@]}"
 do
   echo -n "  Downloading data for ""$node_ip""... "
   check_or_add_to_known_hosts "$node_ip"
-  run_command "$pem_path" "$node_ip" "$this_file_dir""/prepare_data_from_s3.sh"
+
+  # download in parallel
+  pids=()
+  for node_ip in "${node_ips[@]}"
+  do
+    run_command "$pem_path" "$node_ip" "$this_file_dir""/prepare_data_from_s3.sh" "$data_relative_dir" "&"
+    pids[${#pids[@]}]=$!
+  done
+
+  # wait
+  for pid in "${pids[@]}"
+  do
+      wait $pid
+  done
 done
