890a09734c90818cc001939091397ddf35c597e3
Yifei Yang <yiy032@eng.ucsd.edu>
2022-09-21 14:19:33 -0500

    update prepare_data scripts

--- resources/script/fpdb-store/prepare_cluster_data_from_s3.sh
+++ resources/script/fpdb-store/prepare_cluster_data_from_s3.sh
@@ -1,11 +1,13 @@
 # download data from s3 on all storage nodes
 
 # import util
-this_dir=$(dirname "$0")
-util_param_path=$(dirname "$this_dir")"/util_param.sh"
+pushd "$(dirname "$0")" > /dev/null
+cd ..
+util_param_path=$(pwd)"/util_param.sh"
 source "$util_param_path"
-util_func_path=$(dirname "$this_dir")"/util_func.sh"
+util_func_path=$(pwd)"/util_func.sh"
 source "$util_func_path"
+popd > /dev/null
 
 # configurable parameters
 data_relative_dirs=("tpch-sf10")
@@ -20,7 +22,7 @@ done
 pids=()
 for node_ip in "${fpdb_store_ips[@]}"
 do
-  run_command "$pem_path" "$node_ip" "$this_dir""/prepare_data_from_s3.sh" "${data_relative_dirs[@]}" "&"
+  run_command "$pem_path" "$node_ip" "$deploy_dir""/resources/script/fpdb-store/prepare_data_from_s3.sh" "${data_relative_dirs[@]}" "&"
   pids[${#pids[@]}]=$!
 done
 
