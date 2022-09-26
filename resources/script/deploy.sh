3d264f271ce9d15e7c9e52f434177a8ecfb838ba
Yifei Yang <yiy032@eng.ucsd.edu>
2022-09-26 15:24:41 -0500

    Update scripts

---
+++ resources/script/deploy.sh
@@ -0,0 +1,19 @@
+# Script to spread built package to all nodes (both compute and storage, if needed)
+
+# import util
+pushd "$(dirname "$0")" > /dev/null
+util_param_path=$(pwd)"/util_param.sh"
+source "$util_param_path"
+util_func_path=$(pwd)"/util_func.sh"
+source "$util_func_path"
+popd > /dev/null
+
+# storage
+if [ "${use_fpdb_store}" = true ]; then
+  echo "[Deploy FPDB store]"
+  ./fpdb-store/deploy.sh
+fi
+
+# compute
+echo "[Deploy compute layer]"
+./compute/deploy.sh
