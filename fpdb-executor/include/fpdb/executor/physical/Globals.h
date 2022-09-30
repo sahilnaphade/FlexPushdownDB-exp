a261317badeb6b961e97b7c51f9e9c82263b38fa
Yifei Yang <yiy032@eng.ucsd.edu>
2022-09-29 22:39:21 -0500

    Turn off flight comm by default, because there is unknown slowdown with shuffle pushdown for some queries (e.g. q02, q08, q20, q21) on sf10, need to verify on larger data set before re-enabling it.

--- fpdb-executor/include/fpdb/executor/physical/Globals.h
+++ fpdb-executor/include/fpdb/executor/physical/Globals.h
@@ -49,7 +49,7 @@ inline bool ENABLE_FPDB_STORE_BITMAP_PUSHDOWN = false;
 inline bool USE_ARROW_GROUP_BY_IMPL = true;
 inline bool USE_ARROW_HASH_JOIN_IMPL = true;
 inline bool USE_TWO_PHASE_GROUP_BY = true;
-inline bool USE_FLIGHT_COMM = true;
+inline bool USE_FLIGHT_COMM = false;
 inline constexpr int64_t BLOOM_FILTER_MAX_INPUT_SIZE = 20000000;  // won't create bloom filter if input is too large
 
 /**
