2d06058ee6619e97c6b8388e69474ebca4ede8c7
Yifei Yang <yiy032@eng.ucsd.edu>
2023-05-16 17:32:02 -0700

    Saving... (BFS predicate transfer)

--- fpdb-executor/include/fpdb/executor/physical/transform/pred-trans/BFSPredTransOrder.h
+++ fpdb-executor/include/fpdb/executor/physical/transform/pred-trans/BFSPredTransOrder.h
@@ -6,6 +6,7 @@
 #define FPDB_FPDB_EXECUTOR_INCLUDE_FPDB_EXECUTOR_PHYSICAL_TRANSFORM_PRED_TRANS_BFSPREDTRANSORDER_H
 
 #include <fpdb/executor/physical/transform/pred-trans/PredTransOrder.h>
+#include <stack>
 
 namespace fpdb::executor::physical {
 
@@ -16,8 +17,71 @@ public:
   ~BFSPredTransOrder() override = default;
 
 private:
+  enum TransferDir {
+    FORWARD_ONLY,
+    BACKWARD_ONLY,
+    BOTH
+  };
+
+  struct PredTransUnit {
+    uint prePOpId_;       // the prephysical op id of the corresponding FilterableScanPrePOp
+    std::shared_ptr<PhysicalOp> origUpConnOp_;    // the start of the vertical chain, also as the identifier
+    std::shared_ptr<PhysicalOp> currUpConnOp_;    // the end of the vertical chain
+    std::vector<std::pair<std::weak_ptr<PredTransUnit>, TransferDir>> neighbors_;     // neighbors in the join graph
+
+    PredTransUnit(uint prePOpId, const std::shared_ptr<PhysicalOp> &upConnOp):
+      prePOpId_(prePOpId),
+      origUpConnOp_(upConnOp), currUpConnOp_(upConnOp) {}
+
+    size_t hash() const {
+      return std::hash<std::string>()(origUpConnOp_->name());
+    }
+
+    bool equalTo(const std::shared_ptr<PredTransUnit> &other) const {
+      return origUpConnOp_->name() == other->origUpConnOp_->name();
+    }
+  };
+
+  struct PredTransUnitPtrHash {
+    inline size_t operator()(const std::shared_ptr<PredTransUnit> &ptUnit) const {
+      return ptUnit->hash();
+    }
+  };
+
+  struct PredTransUnitPtrPred {
+    inline bool operator()(const std::shared_ptr<PredTransUnit> &lhs, const std::shared_ptr<PredTransUnit> &rhs) const {
+      return lhs->equalTo(rhs);
+    }
+  };
+
+  struct BFSPredTransPair {
+    std::shared_ptr<PredTransUnit> leftPTUnit_;
+    std::shared_ptr<PredTransUnit> rightPTUnit_;
+    TransferDir dir_;
+
+    BFSPredTransPair(const std::shared_ptr<PredTransUnit> &leftPTUnit,
+                     const std::shared_ptr<PredTransUnit> &rightPTUnit,
+                     TransferDir dir):
+      leftPTUnit_(leftPTUnit), rightPTUnit_(rightPTUnit), dir_(dir) {}
+  };
+
   void orderPredTrans(const std::unordered_set<std::shared_ptr<JoinOrigin>, JoinOriginPtrHash,
           JoinOriginPtrPred> &joinOrigins) override;
+
+  void makePTUnits(
+          const std::unordered_set<std::shared_ptr<JoinOrigin>, JoinOriginPtrHash, JoinOriginPtrPred> &joinOrigins);
+  void bfsSearch();
+
+  /**
+   * extra states maintained during transformation
+   */
+  // pred-trans units
+  std::unordered_set<std::shared_ptr<PredTransUnit>, PredTransUnitPtrHash, PredTransUnitPtrPred> ptUnits_;
+  std::shared_ptr<PredTransUnit> rootPTUnit_;
+  double rootPTUnitRowCount_;
+
+  // BFS ordering result, the order is the reverse of BFS search so use a stack here
+  std::stack<std::shared_ptr<BFSPredTransPair>> bfsPredTransOrder_;
 };
 
 }
--- fpdb-executor/src/physical/transform/pred-trans/BFSPredTransOrder.cpp
+++ fpdb-executor/src/physical/transform/pred-trans/BFSPredTransOrder.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <fpdb/executor/physical/transform/pred-trans/BFSPredTransOrder.h>
+#include <queue>
 
 namespace fpdb::executor::physical {
 
@@ -11,6 +12,89 @@ BFSPredTransOrder::BFSPredTransOrder(PrePToPTransformerForPredTrans* transformer
 
 void BFSPredTransOrder::orderPredTrans(
         const std::unordered_set<std::shared_ptr<JoinOrigin>, JoinOriginPtrHash, JoinOriginPtrPred> &joinOrigins) {
+  // make pred-trans units
+  makePTUnits(joinOrigins);
+
+  // order pred-trans by a BFS search
+  bfsSearch();
+
   // TODO
 }
+
+void BFSPredTransOrder::makePTUnits(
+        const std::unordered_set<std::shared_ptr<JoinOrigin>, JoinOriginPtrHash, JoinOriginPtrPred> &joinOrigins) {
+  for (const auto &joinOrigin: joinOrigins) {
+    // transform the join origin ops
+    auto upLeftConnPOps = transformer_->transformDfs(joinOrigin->left_);
+    auto upRightConnPOps = transformer_->transformDfs(joinOrigin->right_);
+
+    // FIXME: currently only support single-partition-table / single-thread-processing
+    if (upLeftConnPOps.size() != 1 || upRightConnPOps.size() != 1) {
+      throw std::runtime_error("Currently only support single-partition-table / single-thread-processing");
+    }
+    auto upLeftConnPOp = upLeftConnPOps[0];
+    auto upRightConnPOp = upRightConnPOps[0];
+
+    // cannot transfer on FULL joins
+    if (joinOrigin->joinType_ == JoinType::FULL) {
+      continue;
+    }
+
+    // make or find predicate transfer units
+    auto leftPTUnit = std::make_shared<PredTransUnit>(joinOrigin->left_->getId(), upLeftConnPOp);
+    auto ptUnitIt = ptUnits_.find(leftPTUnit);
+    if (ptUnitIt == ptUnits_.end()) {
+      ptUnits_.emplace(leftPTUnit);
+    } else {
+      leftPTUnit = *ptUnitIt;
+    }
+    auto rightPTUnit = std::make_shared<PredTransUnit>(joinOrigin->right_->getId(), upRightConnPOp);
+    ptUnitIt = ptUnits_.find(rightPTUnit);
+    if (ptUnitIt == ptUnits_.end()) {
+      ptUnits_.emplace(rightPTUnit);
+    } else {
+      rightPTUnit = *ptUnitIt;
+    }
+
+    // add neighbors
+    TransferDir leftTransferDir = TransferDir::BOTH;
+    TransferDir rightTransferDir = TransferDir::BOTH;
+    if (joinOrigin->joinType_ == JoinType::LEFT) {
+      leftTransferDir = TransferDir::FORWARD_ONLY;
+      rightTransferDir = TransferDir::BACKWARD_ONLY;
+    } else if (joinOrigin->joinType_ == JoinType::RIGHT) {
+      leftTransferDir = TransferDir::BACKWARD_ONLY;
+      rightTransferDir = TransferDir::FORWARD_ONLY;
+    }
+    leftPTUnit->neighbors_.emplace_back(rightPTUnit, leftTransferDir);
+    rightPTUnit->neighbors_.emplace_back(leftPTUnit, rightTransferDir);
+
+    // update root as the ptUnit with the largest table
+    double leftRowCount = joinOrigin->left_->getRowCount();
+    double rightRowCount = joinOrigin->right_->getRowCount();
+    auto largerPTUnit = leftRowCount > rightRowCount ? leftPTUnit : rightPTUnit;
+    double largerPTUnitRowCount = leftRowCount > rightRowCount ? leftRowCount : rightRowCount;
+    if (rootPTUnit_ == nullptr || largerPTUnitRowCount > rootPTUnitRowCount_) {
+      rootPTUnit_ = largerPTUnit;
+      rootPTUnitRowCount_ = largerPTUnitRowCount;
+    }
+  }
+}
+
+void BFSPredTransOrder::bfsSearch() {
+  std::queue<std::shared_ptr<PredTransUnit>> ptUnitsToVisit;
+  ptUnitsToVisit.push(rootPTUnit_);
+  ptUnits_.erase(rootPTUnit_);
+
+  // BFS search
+  while (!ptUnitsToVisit.empty()) {
+    // TODO
+  }
+
+  // elements in ptUnits_ should all be visited
+  if (!ptUnits_.empty()) {
+    throw std::runtime_error("BFS search does not visit all elements in ptUnits_");
+  }
+}
+
 }
